c63310019693c69642f08c51428a512542ee3437
vthakur0905 <vthakur0905@gmail.com>
2025-03-07 00:29:37 +0530

    user sign up route completed

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,62 +1,92 @@
-import express, {Application,  Router, Request, Response } from "express" ;
+import express, { Application, Router, Request, Response } from "express";
 import { userModel } from "../database/db";
 import { signupValidation } from "../middlware/validationMiddleware/signupValidation";
+import { ZodError } from "zod";
+import bcrypt from "bcrypt" ;
+const saltRounds = 10 ;
 
-const app = express() ;
+const app = express();
+app.use(express.json());
 
-function userRoutes(app : Application) {
+function userRoutes(app: Application) {
+  // @ts-ignore
+  app.post("/signup", async (req: Request, res: Response) => {
+    try {
+      const validatedData = signupValidation.parse(req.body);
+      const { firstName, lastName, email, username, password } = validatedData;
 
-    app.post('/signup', async (req : Request, res: Response) => {
+      const existingEmail = await userModel.findOne({ email });
+
+      if (existingEmail) {
+        return res.status(409).json({
+          message: "account already exists",
+        });
+      }
+
+      async function hashPassword (password : string)
+      :Promise<string> {
         
-        
-        const validatedData = signupValidation.parse(req.body) ;       
-        const {firstName, lastName, email, username, password} = validatedData  ;
+        try {
+            return await bcrypt.hash(password, saltRounds) ;
+        }
+        catch(err){
+            console.error("error in hashing password in signup " + err) ;
+            throw new Error("error in hashing password");
+            
+        }
 
+      }
 
-        await userModel.create({
-            firstName , 
-            lastName,
-            email , 
-            username, 
-            password 
+      const hashedPassword :string = await hashPassword(password) ;
 
-        })
+      await userModel.create({
+        firstName : firstName,
+        lastName : lastName,
+        email : email,
+        username : username,
+        password : hashedPassword
+      });
 
-        console.log("created") ;
-        res.status(200).json({
-            message : "created the account"
-        })
+      console.log("created");
+      res.status(201).json({
+        message: "created the account",
+      });
+    } catch (err) {
+      if (err instanceof ZodError) {
+        console.error("failed to validate the input " + err.message);
+        return res.status(411).json({
+          message: "failed to validate data",
+        });
+      }
+      return res.status(401).json({
+        message: "failed to load data",
+      });
+    }
+  });
 
-    })
+  app.post("/signin", (req: Request, res: Response) => {
+    res.send("sign in route");
+  });
 
-    app.post('/signin', (req : Request, res: Response) => {
+  app.post("/content", (req: Request, res: Response) => {
+    res.send(" add content route");
+  });
 
-        res.send("sign in route")
+  app.get("/content", (req: Request, res: Response) => {
+    res.send("get all content route");
+  });
 
-    })
+  app.delete("/content", (req: Request, res: Response) => {
+    res.send("delete a content");
+  });
 
-    app.post('/content', (req : Request , res: Response) => {
-        res.send(" add content route")
-    })
+  app.post("/brain/share", (req: Request, res: Response) => {
+    res.send("get the shareable link here");
+  });
 
-    app.get('/content', (req : Request , res: Response) => {
-        res.send("get all content route")
-    })
-
-    app.delete('/content', (req : Request , res: Response) => {
-        res.send("delete a content")
-    })
-
-    app.post('/brain/share', (req : Request , res : Response) => {
-        res.send("get the shareable link here")
-    })
-
-    app.get('/brain/share', (req : Request , res : Response) => {
-        res.send ("get detail of another's brain")
-    })
-
-
+  app.get("/brain/share", (req: Request, res: Response) => {
+    res.send("get detail of another's brain");
+  });
 }
 
-
-export {userRoutes} ;
+export { userRoutes };
